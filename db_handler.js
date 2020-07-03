1c7f994bd843a8828c2483b26134477332367d8a
dheepssika <35847124+dheepssika@users.noreply.github.com>
2020-07-03 13:01:33 +0100

    Update db_handler

--- db_handler.js
+++ db_handler.js
@@ -1,3 +1,4 @@
+//Loads this section on page reload
 document.addEventListener("DOMContentLoaded", function(event) { 
 	 fetch('http://localhost:5000/left_nav')
 	.then(res => res.json())
@@ -12,54 +13,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
 	document.getElementById("left-tabs-container").innerHTML = TheInnerHTML;})
 });
-function loadDivision(name){ 
-    console.log(name);    
-    if(name=='Remainder'){
-        console.log("div name"+name);        
-        //$('.general-container').hide();
-        //document.getElementById('result-container').appendChild('reminder-container');        
-        $('.reminder-container').show();
-        $('.scheduled-page-container').hide();
-        $('.general-container').hide();
-        document.getElementById('general-container').style.cssText="visibility:hidden";           
-    }else{
-        //console.log("temp");
-        $('.reminder-container').hide();        
-        $('.scheduled-page-container').hide();
-        TheInnerHTML=" ";
-        TheInnerHTML+='<div class="add-new-general-task-container"><div id="add-reminder-button" onClick="displayAddItemPopup(\'scheduled\')">+ New Item</div></div>';
-        document.getElementById("right-general-tabs-container").innerHTML = TheInnerHTML;
-        document.getElementById('general-container').style.cssText="visibility:visible";         
-    }
 
-}
-function displayScheduleTab(){
-    $('.reminder-container').hide();   
-    $('.general-container').hide();
-    $('.scheduled-page-container').show();         
-}
+//The right section has all the rmeinders fetched from the db and the various options avalilable for each reminder
 document.addEventListener("DOMContentLoaded", function(event) { 
-    fetch('http://localhost:5000/right_nav')
-   .then(res => res.json())
-   .then(data=>
-   {var TheInnerHTML='';
-   for (var i=0;i<data.length;i++){		
-       var date=toDate(data[i].creation_date);              
-       var time=toTime(data[i].creation_time);       
-       TheInnerHTML+='<div class="right-nav-tabs" title="'+data[i].list_item+'">'+
-       //'<div class="right-nav-tabs-color-bubble" onclick="markCompleted(\''+data[i].item_name+'\')"></div><div class="right-nav-text">'+data[i].item_name+'</div> <div class="rem-date">'+date+'</div><div class="rem-time">'+time+'</div><div class="details-tab">Details</div></div>';      
-        '<div class="right-nav-tabs-color-bubble" onclick="markCompleted(\''+data[i].item_name+'\')"></div><div class="right-nav-text">'+data[i].item_name+'</div> <div class="rem-date">'+date+'</div><div class="rem-time">'+time+'</div><div class="details-tab" onclick="displayDetailsTab(\''+data[i].item_name+","+data[i].priority+'\')">Details</div></div>';
-   }	
-    TheInnerHTML+='<div class="add-new-reminder-container"><div id="add-reminder-button" onClick="displayAddItemPopup(\'reminder\')">+ New Item</div></div>';
-    document.getElementById("right-tabs-container").innerHTML = TheInnerHTML;})   
+    var payload={
+        task_name:"Reminder"
+    }
+    var data = JSON.stringify( payload );
+    console.log("data:"+data);
+    fetch('http://localhost:5000/right_nav',{
+        method:'POST',
+        body:data,
+        headers:new Headers({'Content-Type': 'application/json'}) 
+     })
+    .then(res => res.json())
+    .then(data=>
+    {var TheInnerHTML='';
+    for (var i=0;i<data.length;i++){		
+        var date=toDate(data[i].creation_date);
+        var time=toTime(data[i].creation_time);
+        TheInnerHTML+='<div class="right-nav-tabs" title="'+data[i].list_item+'">'+        
+            '<div class="right-nav-tabs-color-bubble" onclick="markCompleted(\''+data[i].item_name+'\')"></div><div class="right-nav-text">'+data[i].item_name+'</div> <div class="rem-date">'+date+'</div><div class="rem-time">'+time+'</div><div class="details-tab" onclick="displayDetailsTab(\''+data[i].item_name+","+data[i].priority+'\')">Details</div></div>';
+    }	
+        TheInnerHTML+='<div class="add-new-reminder-container"><div id="add-reminder-button" onClick="displayAddItemPopup(\'reminder\')">+ New Item</div></div>';
+        document.getElementById("right-tabs-container").innerHTML = TheInnerHTML;})
 });
 document.addEventListener("DOMContentLoaded", function(event) { 
     fetch('http://localhost:5000/count_completed')
    .then(res => res.json())
    .then(data=>
-   {var TheInnerHTML='';   
-       TheInnerHTML+='<div class="reminders-count">'+data+" Completed"+'</div>';
-       //console.log(data);              
+   {var TheInnerHTML='';          
+       TheInnerHTML+='<div class="reminders-count">'+data+" Completed"+'</div>';              
        document.getElementById("reminders-count-value").innerHTML = TheInnerHTML;})
 });
 document.addEventListener("DOMContentLoaded", function(event) { 
@@ -67,20 +51,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
    .then(res => res.json())
    .then(data=>
    {var TheInnerHTML='';
-   for (var i=0;i<data.length;i++){		
-       //console.log(data[i]);
+   for (var i=0;i<data.length;i++){		       
        var date=toDate(data[i].creation_date);
-       //console.log("date: "+data[i].creation_date);
        var time=toTime(data[i].creation_time);
        
-      TheInnerHTML+='<div class="scheduled-nav-tabs"><div class="scheduled-task-date">'+date+'</div><div class="scheduled-task-text-container"><div class="scheduled-task-bubble"></div><div class="scheduled-task-text">'+data[i].task_name+'</div><div class="scheduled-task-date-2">'+date+'</div><div class="sheduled-task-time">'+time+'</div></div></div>';
-       //'<div class="right-nav-tabs-color-bubble" onclick="markCompleted(\''+data[i].item_name+'\')"></div><div class="right-nav-text">'+data[i].item_name+'</div> <div class="rem-date">'+date+'</div><div class="rem-time">'+time+'</div><div class="details-tab">Details</div></div>';      
+      TheInnerHTML+='<div class="scheduled-task-date">'+date+'</div><div class="scheduled-nav-tabs"><div class="scheduled-task-text-container"><div class="scheduled-task-bubble"></div><div class="scheduled-task-text">'+data[i].task_name+'</div><div class="scheduled-task-date-2">'+date+'</div><div class="sheduled-task-time">'+time+'</div></div></div>';       
         
    }	
     TheInnerHTML+='<div class="add-new-scheduled-task-container"><div id="add-reminder-button" onClick="displayAddItemPopup(\'scheduled\')">+ New Item</div></div>';
     document.getElementById("right-scheduled-tabs-container").innerHTML = TheInnerHTML;})
-   //console.log(db_right_nav);
 });
+function searchCall(){
+    $('.search-result-container').show();
+    var payload={
+        search_query:document.getElementById('search-box-area').value+'%',
+    }
+    var data=JSON.stringify( payload ); 
+    fetch('http://localhost:5000/search_call',{method: "POST", body: data,headers:{'Content-Type': 'application/json'}})
+    .then(res => res.json())
+    .then(data=>{
+        var TheInnerHTML='';
+        for (var i=0;i<data.length;i++){		
+        var date=toDate(data[i].creation_date);        
+        var time=toTime(data[i].creation_time);        
+        TheInnerHTML+='<div class="right-nav-tabs" title="'+data[i].list_item+'">'+
+         '<div class="right-nav-tabs-color-bubble" onclick="markCompleted(\''+data[i].item_name+'\')"></div><div class="right-nav-text">'+data[i].item_name+'</div> <div class="rem-date">'+date+'</div><div class="rem-time">'+time+'</div><div class="details-tab" onclick="displayDetailsTab(\''+data[i].item_name+","+data[i].priority+'\')">Details</div></div>';
+    }
+    document.getElementById("right-search-tabs-container").innerHTML = TheInnerHTML;
+         
+     $('.reminder-container').hide();
+     $('.general-container').hide();
+     $('.scheduled-page-container').hide();
+     
+    })
+}
+//adding an item with it's resoectine description and color 
 function AddItemToDb()
  {      
 		var payload={
@@ -126,30 +131,37 @@ function toDate(date){
     date=date[2]+'/'+date[1]+'/'+date[0];
     return date;
 }
+//priority can be important or none
 function updatePriority(priority){
-    
+    //console.log(priority[1]);
+    console.log(document.getElementById('priority-title').value);
     priority=priority.split(",");
-    if(priority[1] != document.getElementById('priority-title').value){
+        console.log(priority[1]);
+       // console.log("fn");
         var payload={
             priority_value:''+document.getElementById('priority-title').value,
-            reminder_name:''+priority[0],}
+            item_name:''+priority[0],}
             var data=JSON.stringify( payload );    
-            console.log(data);
+            //console.log("data in prior"+data);
             fetch('http://localhost:5000/update_priority',{method: "POST", body: data,headers:{'Content-Type': 'application/json'}})
             window.location.reload();         
-    }
-    document.getElementById('details-popup-container').style.cssText="visibility:hidden";  
+    
+    $('.details-popup-container').css("visibility","hidden");
      
  }
 
-function addReminderToDb(){
-    var payload={
-	reminder_name:''+document.getElementById('reminder-name').value,
-	reminder_date:''+document.getElementById('reminder-date').value,
-	reminder_time:''+document.getElementById('reminder-time').value}
+function addNewItemToDb(task_name){
+    if(task_name == 'scheduled'){
+        console.log("from db handler");
+    }
+    var payload={   
+	element_name:''+document.getElementById('new-element-name').value,
+	element_date:''+document.getElementById('new-element-date').value,
+    element_time:''+document.getElementById('new-element-time').value,
+    task_name:''+task_name}
     var data=JSON.stringify( payload );    
     console.log(data);
-	fetch('http://localhost:5000/reminder_add_item',{method: "POST", body: data,headers:{'Content-Type': 'application/json'}})
+	fetch('http://localhost:5000/element_add_item',{method: "POST", body: data,headers:{'Content-Type': 'application/json'}})
 	window.location.reload();
  }
  function markCompleted(name){
